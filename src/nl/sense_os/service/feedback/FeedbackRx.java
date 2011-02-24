c67ad7bef07cb56b3e7ea828ae0e5ffea959789f
Steven Mulder <steven@sense-os.nl>
2011-02-24 15:16:24 +0000

    bugfix in feedback checker

--- src/nl/sense_os/service/feedback/FeedbackRx.java
+++ src/nl/sense_os/service/feedback/FeedbackRx.java
@@ -3,12 +3,13 @@ package nl.sense_os.service.feedback;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class FeedbackRx extends BroadcastReceiver {
 
-    @SuppressWarnings("unused")
     private static final String TAG = "FeedbackRx";
     public static final String ACTION_CHECK_FEEDBACK = "nl.sense_os.service.CheckFeedback";
     public static final int REQ_CHECK_FEEDBACK = 2;
@@ -16,6 +17,7 @@ public class FeedbackRx extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Log.d(TAG, "onReceive");
 
         /* set the next check broadcast */
         final Intent alarmIntent = new Intent(ACTION_CHECK_FEEDBACK);
@@ -26,6 +28,10 @@ public class FeedbackRx extends BroadcastReceiver {
         mgr.set(AlarmManager.RTC_WAKEUP, alarmTime, alarmOp);
 
         /* start the feedback check task */
-        context.startService(new Intent(FeedbackChecker.ACTION_CHECK_FEEDBACK));
+        ComponentName component = context.startService(new Intent(
+                FeedbackChecker.ACTION_CHECK_FEEDBACK));
+        if (null == component) {
+            Log.w(TAG, "Could not start feedback checker");
+        }
     }
 }
