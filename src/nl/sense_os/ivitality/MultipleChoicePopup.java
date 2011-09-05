db0391026931948594845da205435c18ad981bec
Steven Mulder <steven@sense-os.nl>
2011-09-05 15:14:35 +0000

    working version of question popups

--- src/nl/sense_os/ivitality/MultipleChoicePopup.java
+++ src/nl/sense_os/ivitality/MultipleChoicePopup.java
@@ -1,18 +1,36 @@
 package nl.sense_os.ivitality;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnShowListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 public class MultipleChoicePopup extends Activity {
 
+    @SuppressWarnings("unused")
+    private static final String TAG = "MultipleChoicePopup";
     private String question;
     private String questionId;
     private String[] answers;
+
+    private AlertDialog dialog;
+    private RadioGroup answerFields;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,16 +46,41 @@ public class MultipleChoicePopup extends Activity {
     protected Dialog onCreateDialog(int id) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("iVitality Question");
-        String msg = questionId + ". " + question + "\n";
+
+        ScrollView scrollView = new ScrollView(this);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        TextView q = new TextView(this);
+        q.setText(question);
+        q.setPadding(20, 5, 5, 5);
+        layout.addView(q);
+
+        answerFields = new RadioGroup(this);
+        answerFields.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(checkedId >= 0);
+            }
+        });
+        int answerId = 1;
         for (String answer : answers) {
-            msg += "\n - " + answer;
+            RadioButton answerField = new RadioButton(this);
+            answerField.setText(answer);
+            answerField.setId(answerId);
+            answerId++;
+            answerFields.addView(answerField);
         }
-        builder.setMessage(msg);
+        layout.addView(answerFields);
+        scrollView.addView(layout);
+
+        builder.setView(scrollView);
         builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
+                submit();
                 setResult(RESULT_OK);
                 MultipleChoicePopup.this.finish();
             }
@@ -57,6 +100,33 @@ public class MultipleChoicePopup extends Activity {
                 MultipleChoicePopup.this.finish();
             }
         });
-        return builder.create();
+        dialog = builder.create();
+        dialog.setOnShowListener(new OnShowListener() {
+
+            @Override
+            public void onShow(DialogInterface dialog) {
+                ((AlertDialog) dialog).getButton(DialogInterface.BUTTON_POSITIVE).setEnabled(false);
+            }
+        });
+        return dialog;
+    }
+
+    private void submit() {
+        int selectionId = answerFields.getCheckedRadioButtonId();
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("question ID", questionId);
+        map.put("question", question.replace("?", "-"));
+        map.put("answer ID", selectionId);
+        map.put("answer", answers[selectionId - 1]);
+        JSONObject json = new JSONObject(map);
+
+        Intent intent = new Intent("nl.sense_os.app.MsgHandler.NEW_MSG");
+        intent.putExtra("sensor_name", "ivitality question");
+        intent.putExtra("sensor_device", "ivitality question");
+        intent.putExtra("timestamp", System.currentTimeMillis());
+        intent.putExtra("data_type", "json");
+        intent.putExtra("value", json.toString());
+        startService(intent);
     }
 }
--- src/nl/sense_os/ivitality/SliderQuestionPopup.java
+++ src/nl/sense_os/ivitality/SliderQuestionPopup.java
@@ -1,20 +1,35 @@
 package nl.sense_os.ivitality;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnCancelListener;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
 
 public class SliderQuestionPopup extends Activity {
 
+    @SuppressWarnings("unused")
+    private static final String TAG = "SliderQuestionPopup";
     private String question;
     private String questionId;
     private int min;
     private int max;
     private double step;
+
+    private SeekBar slider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +47,53 @@ public class SliderQuestionPopup extends Activity {
     protected Dialog onCreateDialog(int id) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("iVitality Question");
-        String msg = questionId + ". " + question + "\n";
-        msg += min + " - " + max + " (" + step + ")";
-        builder.setMessage(msg);
+
+        ScrollView scrollView = new ScrollView(this);
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        TextView q = new TextView(this);
+        q.setText(question);
+        q.setPadding(20, 5, 5, 5);
+        layout.addView(q);
+
+        final TextView sliderSummary = new TextView(this);
+        sliderSummary.setText("" + min);
+        sliderSummary.setPadding(20, 25, 20, 5);
+
+        slider = new SeekBar(this);
+        slider.setPadding(20, 5, 20, 5);
+        slider.setMax((int) Math.round((max - min) / step));
+        slider.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // do nothing
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // do nothing
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                sliderSummary.setText("" + getSliderValue());
+            }
+        });
+
+        layout.addView(sliderSummary);
+        layout.addView(slider);
+        scrollView.addView(layout);
+
+        builder.setView(scrollView);
+
         builder.setPositiveButton(android.R.string.ok, new OnClickListener() {
 
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.dismiss();
                 setResult(RESULT_OK);
+                submit();
                 SliderQuestionPopup.this.finish();
             }
         });
@@ -60,5 +113,27 @@ public class SliderQuestionPopup extends Activity {
             }
         });
         return builder.create();
+    }
+
+    private double getSliderValue() {
+        return (slider.getProgress() * step + min);
+    }
+
+    private void submit() {
+
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("question ID", questionId);
+        map.put("question", question.replace("?", "-"));
+        map.put("answer ID", -1);
+        map.put("answer", getSliderValue());
+        JSONObject json = new JSONObject(map);
+
+        Intent intent = new Intent("nl.sense_os.app.MsgHandler.NEW_MSG");
+        intent.putExtra("sensor_name", "ivitality question");
+        intent.putExtra("sensor_device", "ivitality question");
+        intent.putExtra("timestamp", System.currentTimeMillis());
+        intent.putExtra("data_type", "json");
+        intent.putExtra("value", json.toString());
+        startService(intent);
     }
 }
