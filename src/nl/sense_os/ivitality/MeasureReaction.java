72d321fdfc10f55c7f605a71f1d31998309076be
Steven Mulder <steven@sense-os.nl>
2011-09-26 12:42:05 +0000

    minor tweaks to reaction time game

--- src/nl/sense_os/ivitality/MeasureReaction.java
+++ src/nl/sense_os/ivitality/MeasureReaction.java
@@ -15,63 +15,77 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 public class MeasureReaction extends Activity {
 
-    private class ReactionListener implements OnTouchListener {
-
-        private boolean enabled = false;
-
-        public boolean isEnabled() {
-            return enabled;
-        }
+    /**
+     * Task to flip the view to the red square, to trigger a reaction from the user.
+     */
+    private class FlipTask extends TimerTask {
 
         @Override
-        public boolean onTouch(View v, MotionEvent event) {
+        public void run() {
+            // Log.v(TAG, "Flip!");
+            runOnUiThread(new Runnable() {
 
-            if (enabled) {
-                measureReaction();
-                setEnabled(false);
-            }
-
-            return enabled;
-        }
-
-        public void setEnabled(boolean enabled) {
-            this.enabled = enabled;
+                @Override
+                public void run() {
+                    if (count == NR_OF_TESTS - 1) {
+                        flipper.setDisplayedChild(STARTLE);
+                    } else {
+                        flipper.setDisplayedChild(RED_SQUARE);
+                    }
+                    flipper.forceLayout();
+                    startTime = System.currentTimeMillis();
+                }
+            });
         }
     }
 
     private static final String TAG = "Reaction Measurement";
+
     private static final int NR_OF_TESTS = 5;
+
     private static final int START_SCREEN = 0;
     private static final int BLUE_SQUARE = 1;
     private static final int RED_SQUARE = 2;
     private static final int STARTLE = 3;
+
     private ViewFlipper flipper;
     private long startTime;
+    private long redTime;
+    private long blueTime;
     private int count;
     private int[] times;
-    private final ReactionListener touchListener = new ReactionListener();
+    private final Timer flipTimer = new Timer();
+    private TimerTask flipTask;
 
     private void measureReaction() {
 
-        int reaction = (int) (System.currentTimeMillis() - startTime);
+        if (count < times.length) {
+            int reaction = (int) (System.currentTimeMillis() - startTime);
 
-        Log.v(TAG, "Reaction time: " + reaction + " ms");
-        Toast.makeText(MeasureReaction.this, reaction + " ms", Toast.LENGTH_SHORT).show();
+            // Log.v(TAG, "Reaction time: " + reaction + " ms");
+            Toast.makeText(MeasureReaction.this, reaction + " ms", Toast.LENGTH_SHORT).show();
 
-        times[count] = reaction;
-        count++;
+            times[count] = reaction;
+            count++;
 
-        if (count < NR_OF_TESTS) {
-            startWaiting();
+            if (count < NR_OF_TESTS) {
+                startWaiting();
+            } else {
+                showDialog(0);
+            }
         } else {
-            showDialog(0);
+            Log.w(TAG, "Too many touch events received, ignoring extra entry.");
         }
+    }
+
+    @Override
+    public Object onRetainNonConfigurationInstance() {
+        return new Object[] { times, count };
     }
 
     public void onButtonClick(View v) {
@@ -94,10 +108,15 @@ public class MeasureReaction extends Activity {
         setContentView(R.layout.reaction_game);
 
         flipper = (ViewFlipper) findViewById(R.id.reaction_flipper);
-        flipper.setDisplayedChild(START_SCREEN);
 
-        flipper.getChildAt(RED_SQUARE).setOnTouchListener(touchListener);
-        flipper.getChildAt(STARTLE).setOnTouchListener(touchListener);
+        Object config = getLastNonConfigurationInstance();
+        if (null == config) {
+            flipper.setDisplayedChild(START_SCREEN);
+        } else {
+            times = (int[]) ((Object[]) config)[0];
+            count = (Integer) ((Object[]) config)[1];
+            startWaiting();
+        }
     }
 
     @Override
@@ -150,22 +169,15 @@ public class MeasureReaction extends Activity {
         return builder.create();
     }
 
-    public void onReactionClick(View v) {
-        switch (v.getId()) {
-        case R.id.reaction_view_waiting:
-            Log.d(TAG, "CHEATER!!");
-            break;
-        case R.id.reaction_view_active:
-            // measureReaction();
-            break;
-        default:
-            Log.e(TAG, "Unexpected view clicked for reaction: " + v);
-        }
-    }
-
+    /**
+     * Shows the first screen of the real test. This gives the user some time to figure out what is
+     * happening. After some time, {@link #startWaiting()} is called to truly start the reaction
+     * time.
+     */
     private void startTest() {
         flipper.setDisplayedChild(BLUE_SQUARE);
         times = new int[NR_OF_TESTS];
+        count = 0;
 
         new Timer().schedule(new TimerTask() {
 
@@ -182,27 +194,59 @@ public class MeasureReaction extends Activity {
         }, 2000);
     }
 
-    private void startWaiting() {
-        flipper.setDisplayedChild(BLUE_SQUARE);
+    /**
+     * Receives all touch events for this application. Should be slightly quicker than the touch
+     * listeners for the individual views.
+     */
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (flipper.getDisplayedChild()) {
+        case RED_SQUARE:
+            // fall through to STARTLE
+        case STARTLE:
+            measureReaction();
+            return true;
+        case BLUE_SQUARE:
+            long now = System.currentTimeMillis();
+            if (now < redTime && now > blueTime + 300) {
+                onCheat();
+                return true;
+            }
+        default:
+            return super.dispatchTouchEvent(ev);
+        }
+    }
 
-        new Timer().schedule(new TimerTask() {
+    private void onCheat() {
+        // Log.v(TAG, "CHEATER CHEATER CHEATER");
+        runOnUiThread(new Runnable() {
 
             @Override
             public void run() {
-                runOnUiThread(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        if (count == NR_OF_TESTS - 1) {
-                            flipper.setDisplayedChild(STARTLE);
-                        } else {
-                            flipper.setDisplayedChild(RED_SQUARE);
-                        }
-                        startTime = System.currentTimeMillis();
-                        touchListener.setEnabled(true);
-                    }
-                });
+                Toast.makeText(MeasureReaction.this, R.string.reaction_cheat_warning,
+                        Toast.LENGTH_SHORT).show();
+                startWaiting();
             }
-        }, Math.round(3000 + 1000 * Math.random()));
+        });
+    }
+
+    /**
+     * Shows the normal screen with the blue square. After a random time, the square changes to the
+     * red square or the startle screen, and the user can touch the screen to react.
+     */
+    private void startWaiting() {
+
+        // schedule the task to trigger the reaction after a random amount of time
+        if (null != flipTask) {
+            flipTask.cancel();
+        }
+        flipTask = new FlipTask();
+        long flipDelay = Math.round(3000 + 1000 * Math.random());
+        flipTimer.schedule(flipTask, flipDelay);
+
+        // show the blue screen
+        flipper.setDisplayedChild(BLUE_SQUARE);
+        blueTime = System.currentTimeMillis();
+        redTime = blueTime + flipDelay;
     }
 }
