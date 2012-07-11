03b9c03fb682defb7548d732117d3a595ac89e2a
Steven Mulder <steven@sense-os.nl>
2012-07-11 09:37:19 +0000

    fixed crash on GCM registration when the app does not hold GCM permission

--- src/nl/sense_os/service/commonsense/DefaultSensorRegistrationService.java
+++ src/nl/sense_os/service/commonsense/DefaultSensorRegistrationService.java
@@ -4,58 +4,63 @@ import static nl.sense_os.service.push.GCMReceiver.SENDER_ID;
 
 import java.io.IOException;
 
-
 import org.json.JSONException;
-
-import com.google.android.gcm.GCMRegistrar;
 
 import android.app.IntentService;
 import android.content.Intent;
 import android.util.Log;
 
+import com.google.android.gcm.GCMRegistrar;
+
 public class DefaultSensorRegistrationService extends IntentService {
 
-    private static final String TAG = "DefaultSensorRegistrationService";
-    private SensorRegistrator verifier = new DefaultSensorRegistrator(this);
+	private static final String TAG = "DefaultSensorRegistrationService";
+	private SensorRegistrator verifier = new DefaultSensorRegistrator(this);
 
-    public DefaultSensorRegistrationService() {
-	super("DefaultSensorRegistrationService");
-    }
-
-    @Override
-    protected void onHandleIntent(Intent intent) {
-	String deviceType = SenseApi.getDefaultDeviceType(this);
-	String deviceUuid = SenseApi.getDefaultDeviceUuid(this);
-	registerGCM();
-	if (verifier.verifySensorIds(deviceType, deviceUuid)) {
-	    Log.v(TAG, "Sensor IDs verified");	    
-	} else {
-	    // hopefully the IDs will be checked again
+	public DefaultSensorRegistrationService() {
+		super("DefaultSensorRegistrationService");
 	}
-    }
-    
-    
-    
-    @Override
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		String deviceType = SenseApi.getDefaultDeviceType(this);
+		String deviceUuid = SenseApi.getDefaultDeviceUuid(this);
+		registerGCM();
+		if (verifier.verifySensorIds(deviceType, deviceUuid)) {
+			Log.v(TAG, "Sensor IDs verified");
+		} else {
+			// hopefully the IDs will be checked again
+		}
+	}
+
+	@Override
 	public void onDestroy() {
-	GCMRegistrar.onDestroy(this); // so that it will not leak
-	super.onDestroy();
+		GCMRegistrar.onDestroy(this); // so that it will not leak
+		super.onDestroy();
 	}
 
 	/**
 	 * Register the device to use google GCM
 	 */
-	private void registerGCM() {		
-		GCMRegistrar.checkDevice(this);
-		GCMRegistrar.checkManifest(this);
-		final String registrationId = GCMRegistrar.getRegistrationId(this);
-		
+	private void registerGCM() {
+
+		// get the registration ID
+		String registrationId = null;
+		try {
+			GCMRegistrar.checkDevice(this);
+			GCMRegistrar.checkManifest(this);
+			registrationId = GCMRegistrar.getRegistrationId(this);
+		} catch (IllegalStateException e) {
+			Log.w(TAG, "This application does not have the GCM permission");
+			return;
+		}
+
 		if (registrationId.equals("")) {
 			Log.v(TAG, "Device is not registered to gcm, registering");
 			GCMRegistrar.register(this, SENDER_ID);
-			
+
 		} else {
-			try {								
+			try {
 				SenseApi.registerGCMId(this, registrationId);
 			} catch (IOException e) {
 				Log.d(TAG, "error while registering gcm registration_id");
