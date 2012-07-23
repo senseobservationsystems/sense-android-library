30ebba903b160dc276b384631eaf52223a8e6c09
Steven Mulder <steven@sense-os.nl>
2012-07-23 15:41:19 +0000

    fixed errors in Handler / HandlerThread implementations: handlers are now static

--- src/nl/sense_os/service/states/AbstractStateMonitor.java
+++ src/nl/sense_os/service/states/AbstractStateMonitor.java
@@ -10,67 +10,57 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Message;
 
 public abstract class AbstractStateMonitor extends Service {
 
-    private PendingIntent operation;
-    private final int REQCODE = 0x057A7E;
+	private PendingIntent operation;
+	private final int REQCODE = 0x057A7E;
 
-    /**
-     * Receiver for update alarms.
-     */
-    private final BroadcastReceiver updateReceiver = new BroadcastReceiver() {
+	/**
+	 * Receiver for update alarms.
+	 */
+	private final BroadcastReceiver updateReceiver = new BroadcastReceiver() {
 
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            HandlerThread ht = new HandlerThread("State update thread");
-            ht.start();
-            new Handler(ht.getLooper()) {
-                @Override
-                public void handleMessage(Message msg) {
-                    updateState();
-                    getLooper().quit();
-                }
-            }.sendEmptyMessage(0);
-        }
-    };
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO use intent service to do the work
+			updateState();
+		}
+	};
 
-    @Override
-    public IBinder onBind(Intent arg0) {
-        // binding not possible
-        return null;
-    }
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// binding not possible
+		return null;
+	}
 
-    protected abstract void updateState();
+	protected abstract void updateState();
 
-    protected void startMonitoring(String action, long interval) {
+	protected void startMonitoring(String action, long interval) {
 
-        // register receiver
-        registerReceiver(updateReceiver, new IntentFilter(action));
+		// register receiver
+		registerReceiver(updateReceiver, new IntentFilter(action));
 
-        // start alarms
-        Intent alarm = new Intent(action);
-        operation = PendingIntent.getBroadcast(this, REQCODE, alarm, 0);
-        AlarmManager mgr = (AlarmManager) getSystemService(ALARM_SERVICE);
-        mgr.cancel(operation);
-        mgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, operation);
-    }
+		// start alarms
+		Intent alarm = new Intent(action);
+		operation = PendingIntent.getBroadcast(this, REQCODE, alarm, 0);
+		AlarmManager mgr = (AlarmManager) getSystemService(ALARM_SERVICE);
+		mgr.cancel(operation);
+		mgr.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), interval, operation);
+	}
 
-    protected void stopMonitoring() {
+	protected void stopMonitoring() {
 
-        // unregister receiver
-        try {
-            unregisterReceiver(updateReceiver);
-        } catch (IllegalArgumentException e) {
-            // do nothing
-        }
+		// unregister receiver
+		try {
+			unregisterReceiver(updateReceiver);
+		} catch (IllegalArgumentException e) {
+			// do nothing
+		}
 
-        // stop update alarms
-        AlarmManager mgr = (AlarmManager) getSystemService(ALARM_SERVICE);
-        mgr.cancel(operation);
-    }
+		// stop update alarms
+		AlarmManager mgr = (AlarmManager) getSystemService(ALARM_SERVICE);
+		mgr.cancel(operation);
+	}
 }
