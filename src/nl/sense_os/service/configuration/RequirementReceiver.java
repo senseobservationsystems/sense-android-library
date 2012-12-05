0eb2ceffb4703db1a05b43d1886dd177310c7dbd
Steven Mulder <steven@sense-os.nl>
2012-12-05 11:52:03 +0100

    Fixed documentation; added constant for intent extra key

--- src/nl/sense_os/service/configuration/RequirementReceiver.java
+++ src/nl/sense_os/service/configuration/RequirementReceiver.java
@@ -4,22 +4,25 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-
-
+/**
+ * Entrance class for requirement changes from GCM.<br/>
+ * <br/>
+ * The requirements should be put in an Intent extra of the broadcast. When a new requirement is
+ * received, a ConfigurationService is started and passed the requirements.
+ */
 public class RequirementReceiver extends BroadcastReceiver {
 
-	/**
-	 * Main entrance of the function, start a ConfigurationService and pass
-	 * the requirement
-	 * @param context
-	 * @param intent Intenet contain requirements in JSON format
-	 */
+    /**
+     * Key for Intent extra containing the requirements
+     */
+    public static final String EXTRA_REQUIREMENTS = "requirements";
+
 	@Override
 	public void onReceive(Context context, Intent intent) {						
 		Intent reqsIntent = new Intent(context, ConfigurationService.class);
 		
 		//pass the requirement
-		reqsIntent.putExtra("requirements", intent.getStringExtra("requirements"));
+        reqsIntent.putExtra(EXTRA_REQUIREMENTS, intent.getStringExtra(EXTRA_REQUIREMENTS));
 		context.startService(reqsIntent);
 	}
 }
