07f0003ff3837c672e091f18efefc6971deca5a9
Pim Nijdam <pim.n@xs4all.nl>
2012-12-05 14:48:39 +0100

    Fix getUser, joinGroup and shareSensor.

--- src/nl/sense_os/service/commonsense/SenseApi.java
+++ src/nl/sense_os/service/commonsense/SenseApi.java
@@ -725,27 +725,32 @@ public class SenseApi {
 	 * 
 	 * @param context
 	 *            Context for getting preferences
-	 * @return JSONOBject with user if successfully, false otherwise
+	 * @return JSONOBject with user if successful, false otherwise
 	 * @throws JSONException
 	 *             In case of unparseable response from CommonSense
 	 * @throws IOException
 	 *             In case of communication failure to CommonSense
 	 */
 	public static JSONObject getUser(Context context) throws IOException, JSONException {
+		if (null == authPrefs) {
+			authPrefs = context.getSharedPreferences(SensePrefs.AUTH_PREFS, Context.MODE_PRIVATE);
+		}
 		if (null == mainPrefs) {
 			mainPrefs = context.getSharedPreferences(SensePrefs.MAIN_PREFS, Context.MODE_PRIVATE);
 		}
+
+		String cookie = authPrefs.getString(Auth.LOGIN_COOKIE, null);
 		boolean devMode = mainPrefs.getBoolean(Advanced.DEV_MODE, false);
 		
 		String url = devMode ? SenseUrls.DEV_GET_CURRENT_USER : SenseUrls.GET_CURRENT_USER;
 
 		// perform actual request
-		Map<String, String> response = SenseApi.request(context, url, null, null);
+		Map<String, String> response = SenseApi.request(context, url, null, cookie);
 
 		String responseCode = response.get("http response code");
 		JSONObject result = null;
 		if ("200".equalsIgnoreCase(responseCode)) {
-			result = new JSONObject(response.get("user"));
+			result = new JSONObject(response.get("content")).getJSONObject("user");
 		} else {
 			Log.e(TAG, "Error getting user! Response code: " + responseCode);
 		}
@@ -766,12 +771,15 @@ public class SenseApi {
 	 *             In case of communication failure to CommonSense
 	 */
 	public static Boolean joinGroup(Context context, String groupId) throws JSONException, IOException {
-
+		if (null == authPrefs) {
+			authPrefs = context.getSharedPreferences(SensePrefs.AUTH_PREFS, Context.MODE_PRIVATE);
+		}
 		if (null == mainPrefs) {
 			mainPrefs = context.getSharedPreferences(SensePrefs.MAIN_PREFS, Context.MODE_PRIVATE);
 		}
+
+		String cookie = authPrefs.getString(Auth.LOGIN_COOKIE, null);
 		boolean devMode = mainPrefs.getBoolean(Advanced.DEV_MODE, false);
-		
 		
 		//get userId
 		String userId = getUser(context).getString("id");
@@ -781,19 +789,23 @@ public class SenseApi {
 
 		// create JSON object to POST
 		final JSONObject data = new JSONObject();
+		final JSONArray users = new JSONArray();
+		final JSONObject item = new JSONObject();
 		final JSONObject user = new JSONObject();
 		user.put("id", userId);
-		data.put("user", user);
+		item.put("user", user);
+		users.put(item);
+		data.put("users", users);
 
 		// perform actual request
-		Map<String, String> response = SenseApi.request(context, url, data, null);
+		Map<String, String> response = SenseApi.request(context, url, data, cookie);
 
 		String responseCode = response.get("http response code");
 		Boolean result = false;
 		if ("201".equalsIgnoreCase(responseCode)) {
 			result = true;
 		} else {
-			Log.e(TAG, "Error joining group! Response code: " + responseCode);
+			Log.e(TAG, "Error joining group! Response code: " + responseCode + "Response: " + response);
 			result = false;
 		}
 
@@ -817,10 +829,14 @@ public class SenseApi {
 	 *             In case of communication failure to CommonSense
 	 */
 	public static Boolean shareSensor(Context context, String sensorId, String userId) throws JSONException, IOException {
-
+		if (null == authPrefs) {
+			authPrefs = context.getSharedPreferences(SensePrefs.AUTH_PREFS, Context.MODE_PRIVATE);
+		}
 		if (null == mainPrefs) {
 			mainPrefs = context.getSharedPreferences(SensePrefs.MAIN_PREFS, Context.MODE_PRIVATE);
 		}
+
+		String cookie = authPrefs.getString(Auth.LOGIN_COOKIE, null);
 		boolean devMode = mainPrefs.getBoolean(Advanced.DEV_MODE, false);
 		String url = devMode ? SenseUrls.DEV_SHARE_SENSOR : SenseUrls.SHARE_SENSOR;
 		url = url.replaceFirst("<id>", sensorId);
@@ -832,7 +848,7 @@ public class SenseApi {
 		data.put("user", user);
 
 		// perform actual request
-		Map<String, String> response = SenseApi.request(context, url, data, null);
+		Map<String, String> response = SenseApi.request(context, url, data, cookie);
 
 		String responseCode = response.get("http response code");
 		Boolean result = false;
