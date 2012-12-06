1b56a5106e304ac8287954cec79ebaf925ea38d0
Steven Mulder <steven@sense-os.nl>
2012-12-06 10:37:29 +0100

    Added documentation, fixed code style

--- src/nl/sense_os/service/ambience/MagneticFieldSensor.java
+++ src/nl/sense_os/service/ambience/MagneticFieldSensor.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import org.json.JSONObject;
-
 import nl.sense_os.service.R;
 import nl.sense_os.service.constants.SenseDataTypes;
 import nl.sense_os.service.constants.SensorData.DataPoint;
 import nl.sense_os.service.constants.SensorData.SensorNames;
 import nl.sense_os.service.provider.SNTP;
+
+import org.json.JSONObject;
+
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -24,18 +25,24 @@ import android.hardware.SensorManager;
 import android.os.Handler;
 import android.util.Log;
 
+/**
+ * Represents the magnetic field sensor. Registers itself for updates from the Android
+ * {@link SensorManager}.
+ * 
+ * @author Ted Schmidt <ted@sense-os.nl>
+ */
 public class MagneticFieldSensor implements SensorEventListener {
 
     private static final String TAG = "Sense Magnetic Field Sensor";
-    private static String sensor_display_name = "Magnetic field";
+    private static final String SENSOR_DISPLAY_NAME = "magnetic field";
     private long sampleDelay = 0; // in milliseconds
     private long[] lastSampleTimes = new long[50];
     private Context context;
     private List<Sensor> sensors;
     private SensorManager smgr;
-    private Handler MagneticFieldHandler = new Handler();
-    private Runnable MagneticFieldThread = null;
-    private boolean MagneticFieldSensingActive = false;
+    private Handler magneticFieldHandler = new Handler();
+    private Runnable magneticFieldThread = null;
+    private boolean magneticFieldSensingActive = false;
 
     public MagneticFieldSensor(Context context) {
         this.context = context;
@@ -48,9 +55,7 @@ public class MagneticFieldSensor implements SensorEventListener {
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        // Log.d(TAG, "Accuracy changed...");
-        // Log.d(TAG, "Sensor: " + sensor.getName() + "(" + sensor.getType() + "), accuracy: " +
-        // accuracy);
+        // not used
     }
 
     @Override
@@ -80,21 +85,20 @@ public class MagneticFieldSensor implements SensorEventListener {
     		dataFields.put("z", z);
     		String jsonString = new JSONObject(dataFields).toString();
 
-
             // send msg to MsgHandler
             Intent i = new Intent(context.getString(R.string.action_sense_new_data));
             i.putExtra(DataPoint.DATA_TYPE, SenseDataTypes.JSON);
             i.putExtra(DataPoint.VALUE, jsonString);
             i.putExtra(DataPoint.SENSOR_NAME, sensorName);
-            i.putExtra(DataPoint.DISPLAY_NAME, sensor_display_name);
+            i.putExtra(DataPoint.DISPLAY_NAME, SENSOR_DISPLAY_NAME);
             i.putExtra(DataPoint.SENSOR_DESCRIPTION, sensor.getName());
             i.putExtra(DataPoint.TIMESTAMP, SNTP.getInstance().getTime());
             context.startService(i);
         }
-        if (sampleDelay > 500 && MagneticFieldSensingActive) {
+        if (sampleDelay > 500 && magneticFieldSensingActive) {
             // unregister the listener and start again in sampleDelay seconds
             stopMagneticFieldSensing();
-            MagneticFieldHandler.postDelayed(MagneticFieldThread = new Runnable() {
+            magneticFieldHandler.postDelayed(magneticFieldThread = new Runnable() {
 
                 @Override
                 public void run() {
@@ -104,13 +108,27 @@ public class MagneticFieldSensor implements SensorEventListener {
         }
     }
 
-    public void setSampleDelay(long _sampleDelay) {
-        sampleDelay = _sampleDelay;
+    /**
+     * Sets the delay between samples. The sensor registers itself for periodic sampling bursts, and
+     * unregisters after it received a sample.
+     * 
+     * @param sampleDelay
+     *            Sample delay in milliseconds
+     */
+    public void setSampleDelay(long sampleDelay) {
+        this.sampleDelay = sampleDelay;
     }
 
-    public void startMagneticFieldSensing(long _sampleDelay) {
-        MagneticFieldSensingActive = true;
-        setSampleDelay(_sampleDelay);
+    /**
+     * Starts sensing by registering for updates at the Android SensorManager. The sensor registers
+     * for updates at the rate specified by the sampleDelay parameter.
+     * 
+     * @param sampleDelay
+     *            Delay between samples in milliseconds
+     */
+    public void startMagneticFieldSensing(long sampleDelay) {
+        magneticFieldSensingActive = true;
+        setSampleDelay(sampleDelay);
         for (Sensor sensor : sensors) {
             if (sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
                 // Log.d(TAG, "registering for sensor " + sensor.getName());
@@ -119,21 +137,27 @@ public class MagneticFieldSensor implements SensorEventListener {
         }
     }
 
+    /**
+     * Stops the periodic sampling.
+     */
     public void stopMagneticFieldSensing() {
         try {
-            MagneticFieldSensingActive = false;
+            magneticFieldSensingActive = false;
             smgr.unregisterListener(this);
 
-            if (MagneticFieldThread != null)
-                MagneticFieldHandler.removeCallbacks(MagneticFieldThread);
-            MagneticFieldThread = null;
+            if (magneticFieldThread != null)
+                magneticFieldHandler.removeCallbacks(magneticFieldThread);
+            magneticFieldThread = null;
 
         } catch (Exception e) {
-            Log.e(TAG, e.getMessage());
+            Log.e(TAG, "Failed to stop sampling! " + e.getMessage());
         }
 
     }
 
+    /**
+     * @return The delay between samples in milliseconds
+     */
     public long getSampleDelay() {
         return sampleDelay;
     }
