cf6b0b56615bbc0b6397802800af315058956195
ted <ted@sense-os.nl>
2013-02-26 11:02:34 +0100

    Fixed NumberFormatException by checking for NaN values

--- src/nl/sense_os/service/motion/MotionSensorUtils.java
+++ src/nl/sense_os/service/motion/MotionSensorUtils.java
@@ -55,6 +55,8 @@ public class MotionSensorUtils {
         try {
             for (double value : event.values) {
                 // scale to three decimal precision
+            	if(Double.isNaN(value))
+            		continue;
                 value = BigDecimal.valueOf(value).setScale(3, 0).doubleValue();
 
                 switch (axis) {
