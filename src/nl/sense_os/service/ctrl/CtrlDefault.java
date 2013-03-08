3ececfb141c7fbabf24aae420759a63dffdd352a
kimon <kimon@sense-os.nl>
2013-03-08 15:35:19 +0100

    fix bug

--- src/nl/sense_os/service/ctrl/CtrlDefault.java
+++ src/nl/sense_os/service/ctrl/CtrlDefault.java
@@ -371,9 +371,9 @@ public class CtrlDefault extends Controller {
     		
     		double motion = Math.pow((Math.abs(x - xLast) + Math.abs(y - yLast) + Math.abs(z - zLast)), 2);
             totalMotion += motion;
-            x = xLast;
-            y = yLast;
-            z = zLast;
+            xLast = x;
+            yLast = y;
+            zLast = z;
     	}
     	
     	MotionSensor motionSensor = MotionSensor.getInstance(context);
