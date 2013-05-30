63ad8940005dc3af265657f1185a2d23cc812e39
Pim Nijdam <pim.n@xs4all.nl>
2013-05-30 20:34:25 +0200

    Add sit/stand time aggregation.

--- samples/sense-android-demo/src/nl/sense/demo/NerdsData.java
+++ samples/sense-android-demo/src/nl/sense/demo/NerdsData.java
@@ -7,9 +7,14 @@ import java.util.HashMap;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.util.Log;
+
+import nl.sense_os.cortex.dataprocessor.SitStand;
 import nl.sense_os.platform.SensePlatform;
 import nl.sense_os.service.commonsense.SenseApi;
 import nl.sense_os.service.constants.SensorData.SensorNames;
+import nl.sense_os.service.shared.DataProcessor;
+import nl.sense_os.service.shared.SensorDataPoint;
 
 public class NerdsData {
 	private String TAG = "Realtime data";
@@ -17,9 +22,16 @@ public class NerdsData {
 
 	private AggregateData<String> motion;
 	private AggregateData<String> audioVolume;
+	private AggregateData<String> sitStandTime;
+	
+	private SitStand sitStand;
 
 	public NerdsData(SensePlatform platform) {
 		sensePlatform = platform;
+
+		//instantiate sit/stand AI Module to produce data for the sit/stand sitStandTime aggregator
+		sitStand = new SitStand("activity", sensePlatform.getService().getSenseService());
+		sitStand.enable();
 
 		// motion, in categories "low", "medium", "high". Use accelerometer as
 		// some devices lack linear acceleration
@@ -85,6 +97,37 @@ public class NerdsData {
 			}
 		};
 		audioVolume.importData();
+		
+		/* Sit stand time */
+		// time spent sitting and standing
+		sitStandTime = new AggregateData<String>(sensePlatform,
+				"activity", null) {
+			private Long previousTimestamp;
+
+			@Override
+			public void aggregateSensorDataPoint(JSONObject dataPoint) {
+				try {
+					String bin = dataPoint.getJSONObject("value").getString("value");
+					long timestamp = dataPoint.getLong("date");
+					
+					//add data point to the sensor
+					sensePlatform.addDataPoint("activity", "activity", "sit/stand", "string", bin, timestamp);
+
+					long dt = 0;
+					if (previousTimestamp != null) {
+						dt = Math.abs(timestamp - previousTimestamp);
+						// longer than 5 minutes means missing data
+						if (dt > 5 * 60 * 1000)
+							dt = 0;
+					}
+					previousTimestamp = timestamp;
+					addBinValue(bin, dt);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		};
+		sitStandTime.importData();
 	}
 
 	/* Steps */
@@ -199,13 +242,15 @@ public class NerdsData {
 	 * @return json object containing seconds sitting and seconds standing
 	 */
 	public JSONObject getMySitStandData() {
-		String dummy = "{\"sit\":180.0, \"stand\":3600.0}";
-		try {
-			return new JSONObject(dummy);
-		} catch (JSONException e) {
-			e.printStackTrace();
+		ArrayList<AggregateData<String>.DataValue<String>> hist = sitStandTime.getSorted();
+
+		HashMap<String, Double> totalTimes = new HashMap<String, Double>(
+				hist.size());
+		for (AggregateData<String>.DataValue<String> value : hist) {
+			totalTimes.put(value.bin(), value.sum() / 1000.0); //convert from milliseconds to seconds
 		}
-		return null;
+
+		return new JSONObject(totalTimes);
 	}
 
 	/** Return sit/stand time of the group
