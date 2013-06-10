2ed7180cd3ffc2b51635ce64a7d013dceff86a6c
Steven Mulder <steven@sense-os.nl>
2013-06-10 11:09:05 +0200

    Clean attempt at generating docs using doxygen

--- docs/html/classnl_1_1sense__os_1_1service_1_1constants_1_1_util.js
+++
@@ -1,4 +0,0 @@
-var classnl_1_1sense__os_1_1service_1_1constants_1_1_util =
-[
-    [ "humanReadableByteCount", "classnl_1_1sense__os_1_1service_1_1constants_1_1_util.html#afe040db1b147d711b5c6705205100ccc", null ]
-];
--- docs/html/classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.js
+++
@@ -1,8 +0,0 @@
-var classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils =
-[
-    [ "createJsonValue", "classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.html#a7dd3f3046449b15ea758a74c6149831e", null ],
-    [ "getAvailableMotionSensors", "classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.html#af73dcb19b85598aae8a5614343b3c85c", null ],
-    [ "getSensorHeader", "classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.html#a4e23bfedc7b8b2906c390a0dc7f8449a", null ],
-    [ "getSensorName", "classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.html#a999edecf083e870eee9361f2dc2f6af8", null ],
-    [ "getVector", "classnl_1_1sense__os_1_1service_1_1motion_1_1_motion_sensor_utils.html#a678e2b60ce91978fe68ff0a136d375a7", null ]
-];
