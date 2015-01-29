67164c5c287209a0e86868154744ebaee1380baa
ted <ted@sense-os.nl>
2015-01-29 17:41:43 +0100

    Added encrypted data preferences

--- sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
+++ sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
@@ -2,6 +2,6 @@ package nl.sense_os.service.phonestate;
 
 public abstract class AppInfoVersion { 
 
-	public final static String SENSE_LIBRARY_VERSION = "v3.1.0-25-gb36c388-dirty";
-	public final static String CORTEX_VERSION = "";
+	public final static String SENSE_LIBRARY_VERSION = "v3.1.0-44-g6a33f31-dirty";
+	public final static String CORTEX_VERSION = "1.4.2-rc2_coaching_v1.31-6-g3520162";
 }
