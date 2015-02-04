4f0d3d538cea2de2870f4fb1ca29974edc77a220
ted <ted@sense-os.nl>
2015-02-04 19:16:07 +0100

    Removed warnings

--- sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
+++ sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
@@ -2,6 +2,6 @@ package nl.sense_os.service.phonestate;
 
 public abstract class AppInfoVersion { 
 
-	public final static String SENSE_LIBRARY_VERSION = "v3.1.0-53-g67164c5-dirty";
-	public final static String CORTEX_VERSION = "1.4.2-rc2_coaching_v1.31-53-g2d4cb5f";
+	public final static String SENSE_LIBRARY_VERSION = "v3.1.0-54-g1d31cd8-dirty";
+	public final static String CORTEX_VERSION = "1.4.2-rc2_coaching_v1.31-57-gf3a117e-dirty";
 }
