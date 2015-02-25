a79eacaf6d7efe1f378d6cc5b15a5ba264dd5f93
ted <ted@sense-os.nl>
2015-02-25 16:55:05 +0100

    add missing files

--- sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
+++ sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
@@ -2,6 +2,6 @@ package nl.sense_os.service.phonestate;
 
 public abstract class AppInfoVersion { 
 
-	public final static String SENSE_LIBRARY_VERSION = "v3.3.1_v2-10-g66835ab-dirty";
-	public final static String CORTEX_VERSION = "v1.5.0-rc1_coaching_v3.1-1-gaf841f5";
+	public final static String SENSE_LIBRARY_VERSION = "v3.3.2";
+	public final static String CORTEX_VERSION = "";
 }
