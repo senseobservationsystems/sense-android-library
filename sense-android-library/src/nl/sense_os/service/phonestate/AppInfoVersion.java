f6a9ecb507ed665c2a717aee29bdf203e2aa39bf
ted <ted@sense-os.nl>
2015-02-27 11:00:51 +0100

    Reverted AppInfoVersion.java

--- sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
+++ sense-android-library/src/nl/sense_os/service/phonestate/AppInfoVersion.java
@@ -3,5 +3,5 @@ package nl.sense_os.service.phonestate;
 public abstract class AppInfoVersion { 
 
 	public final static String SENSE_LIBRARY_VERSION = "v3.3.2";
-	public final static String CORTEX_VERSION = "v1.5.0";
+	public final static String CORTEX_VERSION = "";
 }
