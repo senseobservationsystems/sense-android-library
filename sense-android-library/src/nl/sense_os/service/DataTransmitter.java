a1e091c4d00f840612a02bd7f557d76edcc1f85c
kanekin <tatsuya@sense-os.nl>
2015-03-27 18:02:58 +0100

    Cleanup-5. Updated comments in DataTransmitter.

--- sense-android-library/src/nl/sense_os/service/DataTransmitter.java
+++ sense-android-library/src/nl/sense_os/service/DataTransmitter.java
@@ -89,8 +89,7 @@ public class DataTransmitter implements Runnable {
             NetworkInfo wifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
             boolean passed_interval = SystemClock.elapsedRealtime() - mLastTxTime >= mTxInterval;
             
-            //TODO: temporary comment out persistRecentData here, since senseLibs stopped sending data to CommonSense.
-            //If it has not saved data in-memory storage more than the interval, save data into persisted storage
+            //If it has not saved data in in-memory storage to the persisted storage more than the interval, save data into the persisted storage.
             if(passed_interval){
               LocalStorage.getInstance( mContext ).persistRecentData();
             }
